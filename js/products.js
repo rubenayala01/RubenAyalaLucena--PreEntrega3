a84583201e01703860bae6ead3d088e6f6476c08
rubenayala01 <ruben.33ayala@gmail.com>
2023-06-13 22:27:11 -0300

    Preentrega3

---
+++ js/products.js
@@ -0,0 +1,33 @@
+const productos = [
+    { 
+    id: 1,
+    nombre: 'Harina', 
+    precio: 50,
+    img:  'https://images.ecestaticos.com/yYw95f9qCwPLqmG4EFeFXrxPFnM=/0x109:2119x1301/1200x1200/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2F6be%2F87b%2F448%2F6be87b448c2cf27ea55a994c163556d6.jpg',
+    cantidad: 1,
+    },
+
+
+    { 
+    id:2,
+    nombre: 'Cerveza',
+    precio: 100,
+    img: 'https://www.diariodesevilla.es/2021/08/05/vivirensevilla/consumo-cerveza-caracter-social_1598850244_142199149_1200x675.jpg',
+    cantidad: 1,
+    },
+
+    { 
+     id:3,   
+     nombre: 'Galletitas',
+     precio: 150,
+     img:'https://static.misionesonline.news/wp-content/uploads/2017/08/galletitas-7floifn9i7f0.jpg',
+     cantidad: 1,
+    },
+    {
+     id: 3,   
+     nombre: 'Leche', 
+     precio: 250,
+     img:'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/6cd88b3e-3c37-48e3-bdb3-c771a20ce4dc/Derivates/cfb6a345-5bc8-4210-8347-e22758345887.jpg',   
+     cantidad: 1,
+    },
+];
